438e8ee3713e740ab56aa0ad591fd4f9392c0861
Suman <83931935+5h4kun1@users.noreply.github.com>
2023-06-17 09:02:59 +0530

    Create print_even_numbers.py

---
+++ Basic programming/print_even_numbers.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Iterate through each number in the list
+for number in numbers:
+    # Check if the number is even
+    if number % 2 == 0:
+        # Print the even number
+        print(number, end=' ')
