cd6bb0ad33f199588549519c4cc29d8f2092688c
Suman <83931935+5h4kun1@users.noreply.github.com>
2023-06-17 11:03:43 +0530

    Update print_even_numbers.py

--- Basic programming/print_even_numbers.py
+++ Basic programming/print_even_numbers.py
@@ -6,3 +6,19 @@ for number in numbers:
     if number % 2 == 0:
         # Print the even number
         print(number, end=' ')
+
+
+"""
+# Prompt the user to enter a list of numbers
+numbers = input("Enter a list of numbers, separated by commas: ")
+
+# Convert the input string into a list of integers
+numbers = [int(num) for num in numbers.split(",")]
+
+# Iterate through each number in the list
+for number in numbers:
+    # Check if the number is even
+    if number % 2 == 0:
+        # Print the even number
+        print(number, end=' ')
+"""
