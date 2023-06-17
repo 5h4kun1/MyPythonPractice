94956942d3a825028f2367591e8ae9c8cfacbb86
Suman <83931935+5h4kun1@users.noreply.github.com>
2023-06-17 08:39:45 +0530

    Update for_loop.py

--- Basic programming/for_loop.py
+++ Basic programming/for_loop.py
@@ -1,1 +1,23 @@
+# Take input of 10 numbers from the user and save them as a list
+number_list = []
+for i in range(10):
+    number = int(input("Enter number {}: ".format(i + 1)))
+    number_list.append(number)
+
+# Create a function to add two numbers and return the result
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Create a function to add all numbers in a list and print the result
+def add_list_numbers(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    print("Sum of all numbers:", total)
+
+# Example usage:
+result = add_numbers(5, 3)
+print("Result of adding two numbers:", result)
+
+add_list_numbers(number_list)
 
